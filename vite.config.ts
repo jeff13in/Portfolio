b6d9c4ca9eb16c33e357b02c1efe1c6ad7c626b9
jeff13in <ejffinsam14@gmail.com>
2025-06-06 12:37:57 -0400

    base fixed

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Portfolio/', // Ensure this is at the top level
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-  base: '/Portfolio/', // Ensure this is at the top level
 });
